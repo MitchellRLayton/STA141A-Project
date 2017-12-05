52c157783708c905783e51f2aa164776a7828a95
MitchellRLayton <laytonmitchell916@gmail.com>
2017-12-04 22:54:34 -0800

    Final Script Completed 12/4

--- Final Project R Script.R
+++ Final Project R Script.R
@@ -63,7 +63,7 @@ avg_digit(data)
 
 
 #4)---------------------------------------------------------------------
-
+start_time <- Sys.time()
 predict_knn = function(points, train, k, metric) {
     
     #Set up a vector for the predicted classes
@@ -101,10 +101,10 @@ predict_knn = function(points, train, k, metric) {
     #output the vector of predictions
     predictions
 }
-
 #Or try it on the test data
-predict_knn(test,train,90,"minkowski")
-
+predict_knn(test,train,95,"euclidean")
+end_time <- Sys.time()
+end_time - start_time
 
 #5)---------------------------------------------------------------------
 
@@ -134,7 +134,7 @@ cv_error_knn = function(train_data,k,metric) {
 cv_error = cv_error_knn(train,1,"euclidean")
 cv_error
 
-
+dist
 
 #6)---------------------------------------------------------------------
 
@@ -220,6 +220,61 @@ for (i in c(1,3,4)) { #3 best K-values and dist metric
 }
 end_time <- Sys.time()
 end_time - start_time
+
+#8)---------------------------------------------------------------------
+# Analysis In Report.
+
+#9)---------------------------------------------------------------------
+
+start_time <- Sys.time()
+
+actual_test = test[,1]
+test_frame = as.data.frame(actual_test)
+test_eu = c(rep(0,15))
+test_man = c(rep(0,15))
+
+for (i in 1:15) { 
+    
+    pred_eu = predict_knn(test,train,i,"euclidean")
+    pred_man = predict_knn(test,train,i,"manhattan")
+    
+    eu_frame = cbind(pred_eu, test_frame)
+    man_frame = cbind(pred_man, test_frame)
+    
+    error_eu = nrow(subset(eu_frame, pred_eu!= actual_test))/nrow(test)
+    error_man = nrow(subset(man_frame, pred_man!= actual_test))/nrow(test)
+    
+    test_eu[i] = error_eu
+    test_man[i] = error_man
+    
+}
+end_time <- Sys.time()
+end_time - start_time
+
+
+x = seq(1,15,1)
+data2 = as.data.frame(cbind(x,test_eu,test_man))
+names(data2) = c("k","Euclidean","Manhattan")
+data2 = melt(data2, id = "k")
+names(data2) = c("k","Distance Metric","test_set_Error")
+p2 = ggplot(data2) + 
+    geom_point(aes(x = k, y = test_set_Error, colour = `Distance Metric`), size = 4, shape = 19) +
+    geom_line(aes(x = k, y = test_set_Error, colour = `Distance Metric`), size = 1) +
+    geom_text_repel(aes(x=k,y=test_set_Error,label=round(test_set_Error,5)),size=3.15) +
+    theme_calc() +
+    scale_x_continuous("k", breaks = c(seq(1,15,1))) +
+    scale_y_continuous("Test Set Error Rates") +
+    labs(title = "K-Dependent Test Set Error Rates") + 
+    theme(axis.title.x = element_text(size = rel(1.25))) +
+    theme(axis.title.y = element_text(size = rel(1.15))) +
+    theme(axis.text.x = element_text(size = rel(1.25))) +
+    theme(axis.text.y = element_text(size = rel(1.25))) + 
+    theme(title = element_text(size = rel(1.5))) +
+    theme(legend.title = element_text(size = rel(.85))) +
+    theme(legend.text = element_text(size = rel(.9)))
+
+
+p2
 
 
 
